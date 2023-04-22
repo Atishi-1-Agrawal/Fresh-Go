0666e88a2fe391919ab2bfe2cd6f8ce7fc1ecea2
Atishi-1-Agrawal <e21cseu0848@bennett.edu.in>
2023-04-22 16:17:28 +0530

    feat(app): add viewBinding to buildFeatures in build.gradle feat(app): add firebase-database-ktx dependency to build.gradle fix(MainActivity.kt): remove finish() call in onCreate method feat(userdataActivity2.kt): add Firebase Realtime Database support and save user data The viewBinding feature is now enabled in the build.gradle file, which allows for easier access to views in the app. The firebase-database-ktx dependency is added to allow for Firebase Realtime Database support. The finish() call in the onCreate method of MainActivity.kt is removed to prevent the activity from closing immediately. Firebase Realtime Database support is added to userdataActivity2.kt, allowing user data to be saved to the database.

--- app/src/main/java/com/example/dti/freshgo/userdataActivity2.kt
+++ app/src/main/java/com/example/dti/freshgo/userdataActivity2.kt
@@ -3,11 +3,16 @@ package com.example.dti.freshgo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.widget.Toast
 import com.example.dti.freshgo.databinding.ActivityUserdata2Binding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class userdataActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserdata2Binding
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserdata2Binding.inflate(layoutInflater)
@@ -15,9 +20,25 @@ class userdataActivity2 : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+
+
+
         binding.Save.setOnClickListener {
-            val intent = Intent(this,freebasicActivity::class.java)
-            startActivity(intent)
+            val name = binding.etName.text.toString()
+            val contact = binding.etContact.text.toString()
+            val dob = binding.etDOB.text.toString()
+            val gender = binding.etGender.text.toString()
+
+
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            val User = User(name,contact,dob,gender)
+            database.child(name).setValue(User).addOnSuccessListener {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
+                .addOnFailureListener {
+                Toast.makeText(this,"Something Went Wrong!",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
