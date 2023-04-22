b0e254b933c5d3c2a62c0f6a95f498c5e631fde5
Atishi-1-Agrawal <e21cseu0848@bennett.edu.in>
2023-04-22 23:51:50 +0530

    feat(ProductAdapter.kt): add click listener to product item to open product details activity The click listener is added to the product item in the ProductAdapter class. When the item is clicked, it opens the ProductDetailsActivity and passes the product ID as an extra to the activity. This allows the user to view the details of the product they clicked on.

--- app/src/main/java/com/example/dti/freshgo/adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/dti/freshgo/adapter/ProductAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.dti.freshgo.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.dti.freshgo.activity.ProductDetailsActivity
 import com.example.dti.freshgo.databinding.LayoutProductItemBinding
 import com.example.dti.freshgo.model.AddProductModel
 
@@ -33,6 +35,12 @@ class ProductAdapter(val context: Context, val list: ArrayList<AddProductModel>)
 
 
         holder.binding.button.text = data.productSp
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ProductDetailsActivity::class.java)
+            intent.putExtra("id",list[position].productId)
+            context.startActivity(intent)
+        }
     }
 
 }
