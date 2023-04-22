5d9ce24711c6801830793e44cdc9d489cabec8b6
Atishi-1-Agrawal <e21cseu0848@bennett.edu.in>
2023-04-23 01:21:29 +0530

    feat(app): add Room database and coroutines dependencies feat(ProductDetailsActivity.kt): add cart functionality The Room database and coroutines dependencies were added to the app's build.gradle file. The Room database is used to store product data, and coroutines are used to perform database operations asynchronously. In the ProductDetailsActivity.kt file, the cart functionality was added. The user can now add a product to the cart or go to the cart if the product is already in the cart. The product data is stored in the Room database.

--- app/src/main/java/com/example/dti/freshgo/activity/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/dti/freshgo/activity/ProductDetailsActivity.kt
@@ -1,15 +1,23 @@
 package com.example.dti.freshgo.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView.ScaleType
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
+import com.example.dti.freshgo.MainActivity
 import com.example.dti.freshgo.R
 import com.example.dti.freshgo.databinding.ActivityProductDetailsBinding
+import com.example.dti.freshgo.roomdb.AppDatabase
+import com.example.dti.freshgo.roomdb.ProductDao
+import com.example.dti.freshgo.roomdb.ProductModel
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ProductDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductDetailsBinding
@@ -26,20 +34,65 @@ class ProductDetailsActivity : AppCompatActivity() {
         Firebase.firestore.collection("products")
             .document(proId!!).get().addOnSuccessListener {
                 val list = it.get("productImages") as ArrayList<String>
-                binding.textView7.text = it.getString("productName")
-                binding.textView8.text = it.getString("productSp")
-                binding.textView9.text = it.getString("productDescription")
+                val name = it.getString("productName")
+                val productSp = it.getString("productSp")
+                val productDes = it.getString("productDescription")
+
+                binding.textView7.text = name
+                binding.textView8.text = productSp
+                binding.textView9.text = productDes
 
                 val slidList = ArrayList<SlideModel>()
-
                 for(data in list){
                     slidList.add(SlideModel(data, ScaleTypes.FIT))
                 }
+
+                cartAction(proId, name,productSp, it.getString("productCoverImg "))
 
                 binding.imageSlider.setImageList(slidList)
 
             }.addOnFailureListener {
                 Toast.makeText(this,"Something Went Wrong!",Toast.LENGTH_SHORT).show()
             }
+    }
+
+    private fun cartAction(proId: String, name: String?, productSp: String?, coverImg: String?) {
+
+        val productDao = AppDatabase.getInstance(this).productDao()
+
+
+        if(productDao.isExit(proId) != null){
+            binding.textView10.text = "Go To Cart"
+        }else{
+            binding.textView10.text = "Add To Cart"
+        }
+
+        binding.textView10.setOnClickListener {
+            if(productDao.isExit(proId) != null){
+                openCart()
+            }else{
+                addToCart(productDao, proId, name, productSp, coverImg)
+            }
+        }
+    }
+
+    private fun addToCart(productDao: ProductDao, proId: String, name: String?, productSp: String?, coverImg: String?) {
+        val data = ProductModel(proId, name, coverImg, productSp)
+        lifecycleScope.launch(Dispatchers.IO){
+            productDao.insertProduct(data)
+            binding.textView10.text = "Go To Cart"
+
+        }
+    }
+
+    private fun openCart() {
+        val preference = this.getSharedPreferences("info", MODE_PRIVATE)
+        val editor = preference.edit()
+        editor.putBoolean("isCart", true)
+        editor.apply()
+
+
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 }
