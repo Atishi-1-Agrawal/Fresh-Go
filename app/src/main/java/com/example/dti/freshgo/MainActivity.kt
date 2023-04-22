1272bca50f843fbfdc33925ffe76ea8a2f1f895d
Atishi-1-Agrawal <e21cseu0848@bennett.edu.in>
2023-04-22 17:02:44 +0530

    feat(app): add androidx.navigation.safeargs.kotlin plugin and dependencies for Navigation, Smooth Bottom Bar, and Firebase The androidx.navigation.safeargs.kotlin plugin is added to the build.gradle file. Navigation, Smooth Bottom Bar, and Firebase dependencies are added to the dependencies block in the build.gradle file. This allows the application to use the Navigation library, Smooth Bottom Bar, and Firebase services.
    feat(app): add CartFragment
    A new fragment named CartFragment is added to the application. This fragment will be used to display the user's cart.
    
    fix(MainActivity.kt): set up bottom navigation bar with NavController and add onDestinationChangedListener
    The bottom navigation bar is set up with the NavController in MainActivity.kt. An onDestinationChangedListener is added to change the title of the activity based on the current destination.
    
    feat: add HomeFragment, MoreFragment, and their respective layouts
    This commit adds two new fragments, HomeFragment and MoreFragment, along with their respective layouts, fragment_home.xml and fragment_more.xml. These fragments will be used to display content in the application.
    
    feat: add moreFragment layout, bottom navigation menu, and navigation graph
    The commit adds a new layout file for the MoreFragment, a bottom navigation menu file, and a navigation graph file. The MoreFragment layout file contains a TextView with the text "My Dashboard". The bottom navigation menu file contains three items: Home, Cart, and Profile. The navigation graph file contains three fragments: HomeFragment, CartFragment, and MoreFragment. The MoreFragment is now accessible via the bottom navigation menu. The commit also updates the colorPrimary and colorPrimaryVariant values in the themes.xml file to match the app's design. Finally, the build.gradle and settings.gradle files are updated to include the necessary dependencies for the navigation graph.

--- app/src/main/java/com/example/dti/freshgo/MainActivity.kt
+++ app/src/main/java/com/example/dti/freshgo/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.dti.freshgo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.dti.freshgo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav)
+
+        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.cartFragment -> "Cart"
+                    R.id.moreFragment -> "My DashBoard"
+                    else -> "FreshGo "
+                }
+            }
+
+        })
     }
 }
